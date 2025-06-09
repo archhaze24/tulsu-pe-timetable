474cdf7be5dde4cf3bdbf9b9650c2119557aa66e
Kirill Belolipetsky <archhaze24@gmail.com>
2025-06-10 02:37:06 +0300

    feat(backend): example sqlite migration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,18 @@
 mod api;
+mod db;
 
-use tauri_helper::tauri_collect_commands;
 use crate::api::handlers::*;
- 
-// мы не компилируемся для мобилок, но на всякий случай
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+use crate::db::init::init_db;
+use tauri_helper::tauri_collect_commands;
+
+pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let pool = init_db("test.db").await?; // todo: пока что хардкод, поменять под конфиг
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri_collect_commands!())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
